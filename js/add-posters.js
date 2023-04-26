c775d9884126fe2603f09c6e313fef2dbdd202b6
PolinaT <timoninapa@gmail.com>
2023-04-26 22:15:23 +0300

    +posterProps

--- js/add-posters.js
+++ js/add-posters.js
@@ -17,10 +17,81 @@ const addPosters = (postersData) => {
   const simularListFragment = document.createDocumentFragment();
 
   const addPoster = ({ author, offer}) => {
+    const posterProps = [
+      {
+        avatar: {
+          condition: author.avatar,
+          querySelector: '.popup__avatar',
+          targetProp: 'src',
+          targetValue: author.avatar,
+        }
+      }, {
+        title: {
+          condition: offer.title,
+          querySelector: '.popup__title',
+          targetProp: 'textContent',
+          targetValue: offer.title,
+        }
+      }, {
+        address: {
+          condition: offer.address,
+          querySelector: '.popup__text--address',
+          targetProp: 'textContent',
+          targetValue: offer.address,
+        }
+      }, {
+        price: {
+          condition: offer.price,
+          querySelector: '.popup__text--price',
+          targetProp: 'textContent',
+          targetValue: `${ offer.price } ₽/ночь`,
+        }
+      }, {
+        type: {
+          condition: offer.type,
+          querySelector: '.popup__type',
+          targetProp: 'textContent',
+          targetValue: getOfferType(offer.type),
+        }
+      }, {
+        roomsAndGuests: {
+          condition: offer.rooms && offer.guests,
+          querySelector: '.popup__text--capacity',
+          targetProp: 'textContent',
+          targetValue: `${ offer.rooms } комнаты для ${ offer.guests } гостей`,
+        }
+      }, {
+        checkiInOut: {
+          condition: offer.checkin && offer.checkout,
+          querySelector: '.popup__text--time',
+          targetProp: 'textContent',
+          targetValue: `Заезд после ${ offer.checkin }, выезд до ${ offer.checkout }`,
+        }
+      }, {
+        description: {
+          condition: offer.description,
+          querySelector: '.popup__description',
+          targetProp: 'textContent',
+          targetValue: offer.description,
+        }
+      }
+    ];
+
     const posterElement = newPosterTemplate.cloneNode(true);
 
 
-    if (author.avatar) {
+    const renderPosterProp = (condition, querySelector, targetProp, targetValue) => {
+      const targetElement = posterElement.querySelector(querySelector);
+      if (condition) {
+        targetElement[targetProp] = targetValue;
+      } else {
+        targetElement.remove();
+      }
+    };
+
+    posterProps.forEach((item) => Object.values(item).map((value) => renderPosterProp(value.condition, value.querySelector, value.targetProp, value.targetValue)));
+
+    /*if (author.avatar) {
       posterElement.querySelector('.popup__avatar').src = author.avatar;
     } else {
       posterElement.querySelector('.popup__avatar').remove();
@@ -66,7 +137,7 @@ const addPosters = (postersData) => {
       posterElement.querySelector('.popup__description').textContent = offer.description;
     } else {
       posterElement.querySelector('.popup__description').remove();
-    }
+    }*/
 
     if (offer.features) {
       const featuresList = posterElement.querySelector('.popup__features');
